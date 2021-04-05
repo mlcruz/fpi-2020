d6e163d4df43a1c0d048503b71b971fcb789ec4a
Matheus Cruz <mlcruz@inf.ufrgs.br>
2021-04-05 00:05:13 -0300

    brilho

---
+++ trab3/py/contrast.py
@@ -0,0 +1,43 @@
+import numpy as np
+import cv2 as cv
+import math
+
+cap = cv.VideoCapture(0)
+
+def on_trackbar(val):
+    None
+
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
+
+title = "Trabalho 3 - Brilho e contraste"
+cv.namedWindow(title)
+
+cv.createTrackbar("Brilho", title , 0, 256, on_trackbar)
+cv.createTrackbar("Contraste", title , 0, 100, on_trackbar)
+
+while True:
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    brilho = cv.getTrackbarPos("Brilho", title)
+    contraste = cv.getTrackbarPos("Contraste", title)
+
+    # if frame is read correctly ret is True
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+    
+    scaled = cv.convertScaleAbs(frame, alpha=(1+ (contraste / 10.0)), beta=brilho)
+
+    cv.imshow("Original", frame)
+    cv.imshow(title, scaled)
+
+    if cv.waitKey(1) == ord('q'):
+        break
+
+
+# When everything done, release the capture
+cap.release()
+cv.destroyAllWindows()
